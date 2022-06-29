3c386259a4322cb7b62f3b6f22ff65f122b24955
Igor <523a@mail.ru>
2022-06-29 19:33:50 +0300

    Add files via upload

--- data/code.py
+++ data/code.py
@@ -1,7 +1,19 @@
 import numpy as np
 import pandas as pd
 from js import fetch
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import HistGradientBoostingClassifier, HistGradientBoostingRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_transformer
+from sklearn.compose import make_column_selector
+from sklearn.preprocessing import OrdinalEncoder, LabelEncoder, StandardScaler
+from sklearn.metrics import accuracy_score, mean_absolute_percentage_error
+from sklearn.metrics import classification_report
 import re
 import string
 import joblib
-print("Привет!")
+import sklearn
+
+
+
+print("All Ok!")
