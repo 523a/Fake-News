38adc68c14923998b56acbe86678f3f4e9940677
Igor <523a@mail.ru>
2022-06-29 17:33:54 +0300

    Update code.py

--- data/code.py
+++ data/code.py
@@ -1,1 +1,10 @@
 print("Привет Хер!!!!!!")
+from js import fetch
+URL = "https://raw.githubusercontent.com/523a/Fake-News/main/data/lt1.csv"
+res = await fetch(URL)
+text = await res.text()
+filename = 'lt1.csv'
+with open(filename, 'w') as f:
+    f.write(text)
+lt1 = pd.read_csv(filename)
+print(lt1)
