83c827543c1b357e74df94e497a3897b78d022e4
Igor <523a@mail.ru>
2022-06-29 17:47:59 +0300

    Add files via upload

--- data/code.py
+++ data/code.py
@@ -1,9 +1,4 @@
 import numpy as np
+import pandas as pd
 from js import fetch
-URL = "https://raw.githubusercontent.com/523a/Fake-News/main/data/lt1.csv"
-res = await fetch(URL)
-text = await res.text()
-filename = 'lt1.csv'
-with open(filename, 'w') as f:
-    f.write(text)
-lt1 = pd.read_csv(filename)
+f"{sklearn.__version__ = }, {np.__version__ = }, {pd.__version__ = }"
