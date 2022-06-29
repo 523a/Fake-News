00c209783c32d2e2e03842b4ec93d124995cb676
Igor <523a@mail.ru>
2022-06-29 19:43:35 +0300

    Add files via upload

--- data/code.py
+++ data/code.py
@@ -15,5 +15,23 @@ import joblib
 import sklearn
 
 
+from js import fetch
+import os
+from pprint import pprint
+import json
+apikey = os.getenv('R7F8JAZUaZUEgbei45EDdUY9X74ECHWC', 'tfInspSSsKz5HIiPy4BTVc7lNSCGfGs8')
+
+# Top Stories:
+# https://developer.nytimes.com/docs/top-stories-product/1/overview
+section = "world"
+URL = f"https://api.nytimes.com/svc/topstories/v2/{section}.json?api-key={apikey}"
+res = await fetch(URL)
+json1 = await res.json()
+
+
+pprint(json1.results[2].abstract)
+pprint(json1.num_results)
+
+
 
 print("All Ok!")
