fdd0fb6675c06559b12c859cb3e7ea0e9b918c68
Igor <523a@mail.ru>
2022-06-30 09:02:04 +0300

    Update cod.py

--- data/cod.py
+++ data/cod.py
@@ -8,9 +8,10 @@ import js
 csv_content = js.csvContent
 print("1")
 reader = csv.DictReader(io.StringIO(csv_content))
+print("-2-")
 headers = []
 rows = []
-print("-2-")
+
 headers = reader.fieldnames
 for line in reader:
     rows.append(line)
