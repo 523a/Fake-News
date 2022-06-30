c249b0ef42032a13d2ca8925b9330483bc1374cf
Igor <523a@mail.ru>
2022-06-30 09:06:23 +0300

    Update cod.py

--- data/cod.py
+++ data/cod.py
@@ -3,19 +3,28 @@ import io
 import csv
 import js
 
+from js import fetch
+URL = "https://raw.githubusercontent.com/523a/Fake-News/main/data/lt1.csv"
 
+res = await fetch(URL)
 
-csv_content = js.csvContent
+text = await res.text()
+filename = 'lt1.csv'
+with open(filename, 'w') as f:
+    f.write(text)
+
+lt1 = pd.read_csv(filename)
 print("1")
-reader = csv.DictReader(io.StringIO(csv_content))
 print("-2-")
+
+'''csv_content = js.csvContent
+reader = csv.DictReader(io.StringIO(csv_content))
 headers = []
 rows = []
-
 headers = reader.fieldnames
 for line in reader:
     rows.append(line)
-
+'''
 
 
 
