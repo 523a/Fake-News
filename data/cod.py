55c38777c746e4f800bf2d6565243232859612e2
Igor <523a@mail.ru>
2022-06-30 08:52:45 +0300

    Add files via upload

---
+++ data/cod.py
@@ -0,0 +1,19 @@
+import io
+import csv
+import js
+
+csv_content = js.csvContent
+
+reader = csv.DictReader(io.StringIO(csv_content))
+headers = []
+rows = []
+print("-1-")
+headers = reader.fieldnames
+for line in reader:
+    rows.append(line)
+
+
+
+
+
+print("All Ok!")
