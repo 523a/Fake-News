4e8e11d1e2c524bd174e5d0022b0ba0fdaeec1aa
Igor <523a@mail.ru>
2022-06-30 12:14:51 +0300

    Update cod.py

--- data/cod.py
+++ data/cod.py
@@ -3,25 +3,25 @@ import io
 import csv
 import js
 
+
+
+
+'''
 from js import fetch
 URL ="https://raw.githubusercontent.com/523a/Fake-News/main/data/lt1.csv"
 
 myData = [["first_name", "second_name", "Grade"],
           ['Alex', 'Brian', 'A'],
           ['Tom', 'Smith', 'B']]
- 
-myFile = open('ex1.csv', 'w')
+ myFile = open('ex1.csv', 'w')
 with myFile:
     writer = csv.writer(myFile)
     writer.writerows(myData)
-
 lt = pd.read_csv("ex1.csv")
-print(lt)
 
 print("Writing complete 4")
 
-
-'''csv_content = js.csvContent
+csv_content = js.csvContent
 reader = csv.DictReader(io.StringIO(csv_content))
 headers = []
 rows = []
