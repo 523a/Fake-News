4cf91f7ce98057f8663de000ed39aa3a849d7b5a
Igor <523a@mail.ru>
2022-06-30 14:22:41 +0300

    Update cod.py

--- data/cod.py
+++ data/cod.py
@@ -17,8 +17,9 @@ apikey = os.getenv('R7F8JAZUaZUEgbei45EDdUY9X74ECHWC', 'tfInspSSsKz5HIiPy4BTVc7l
 section = "world"
 
 url4 = f"https://api.nytimes.com/svc/topstories/v2/{section}.json?api-key={apikey}"
+print(url4)
 
-json1 = json.dump(open_url(url4))
+json1 = json.(open_url(url4))
 
 pprint(json1.results[2].abstract)
 pprint(json1.num_results)
@@ -30,29 +31,3 @@ print("All Ok 9 !")
 
 
 
-
-'''
-from js import fetch
-URL ="https://raw.githubusercontent.com/523a/Fake-News/main/data/lt1.csv"
-
-myData = [["first_name", "second_name", "Grade"],
-          ['Alex', 'Brian', 'A'],
-          ['Tom', 'Smith', 'B']]
- myFile = open('ex1.csv', 'w')
-with myFile:
-    writer = csv.writer(myFile)
-    writer.writerows(myData)
-lt = pd.read_csv("ex1.csv")
-
-print("Writing complete 4")
-
-csv_content = js.csvContent
-reader = csv.DictReader(io.StringIO(csv_content))
-headers = []
-rows = []
-headers = reader.fieldnames
-for line in reader:
-    rows.append(line)
-
-print("All Ok!")
-'''
