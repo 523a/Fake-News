3bc37b4c287865ac81c6fe64887a21d37b5e7f87
Igor <523a@mail.ru>
2022-07-05 09:47:38 +0300

    Update cod.py

--- data/cod.py
+++ data/cod.py
@@ -6,7 +6,7 @@ import js
 import js
 
 div = js.document.createElement("div")
-div.innerHTML = "<h1> with Python </h1>"
+div.innerHTML = "<h1> This element was with Python </h1>"
 js.document.body.prepend(div)
 
 from pyodide.http import open_url
