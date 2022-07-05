88fe242d8d218cf8a1c9be8f82db95088fa5840b
Igor <523a@mail.ru>
2022-07-05 09:00:08 +0300

    Update cod.py

--- data/cod.py
+++ data/cod.py
@@ -34,7 +34,7 @@ print(r)
 i=0
 while i < json1["num_results"]:
     nnn = json1["results"][i]["abstract"]
-    div.innerHTML = "<p>"+nn+"</p>"
+    div.innerHTML = "<p>"+nnn+"</p>"
     js.document.body.prepend(div)
     
     print((nnn),i)
